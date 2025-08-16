62ef03444c2a281af606f8b7a03c45326ae61280
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-08-16 09:48:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ git clone https://github.com/MusicPlayerDaemon/MPD.git input
 cd input
 mkdir -p output/android
 cd ./output/android
-python3 ../../android/build.py $ANDROID_SDK_ROOT $ANDROID_NDK_HOME arm64-v8a --buildtype=release -Db_ndebug=false
+python3 ../../android/build.py $ANDROID_SDK_ROOT $ANDROID_NDK_HOME arm64-v8a --buildtype=debugoptimized -Db_ndebug=true
 cd -
 cd ./android
 export JAVA_HOME=$JAVA_HOME_17_X64
-./gradlew --no-daemon --stacktrace assembleArm64-v8aRelease
+./gradlew --no-daemon --stacktrace assembleArm64-v8aDebug
 mkdir ../../output
-cp app/build/outputs/apk/arm64-v8a/release/*.apk ../../output
+cp app/build/outputs/apk/arm64-v8a/debug/*.apk ../../output
 zipalign -p 4 ../../output/*.apk ../../output/aligned.apk
 apksigner sign --ks-key-alias lob --ks ../../sign.keystore --ks-pass pass:369852 --key-pass pass:369852 ../../output/aligned.apk
 mkdir ../../prebuilt
